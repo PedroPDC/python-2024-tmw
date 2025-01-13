d642164f215f8a281aaa307e0b10e3e33f546b5a
Pedro Paulo <0901dantaspedro@gmail.com>
2025-01-12 23:57:54 -0300

    Variaveis e input

---
+++ dia02/input.py
@@ -0,0 +1,18 @@
+# %%
+nome = input("Entre com o seu nome: \nEsperando... ")
+print(nome)
+
+# %%
+a = input("Entre com o valor de A: ")
+a = int(a)
+
+b = int(input("Entre com o valor de B: "))
+
+soma = a + b
+print(soma)
+
+# %%
+print("Pedro" + "Paulo") # -> "PedroPaulo"
+print("10" + "30") # -> 1030
+
+# %%
---
+++ dia02/variaveis.py
@@ -0,0 +1,18 @@
+# %%
+nome = "Pedro Paulo"
+print(nome)
+
+# %%
+print("Seja bem-vindo,", nome)
+
+# %%
+nome = "Téo"
+print(nome)
+
+# %%
+a = 10
+b = 20
+resultado = a + b
+print(resultado)
+
+# %%
